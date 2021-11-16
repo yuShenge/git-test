6b03c1d306d89b813e0c6ed67da6649b07e98cb6
yuSheng <yuSheng@qq.com>
2021-11-16 23:15:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * date: 2021/11/16 23:12<br/>
+ *
+ * @author 23972<br />
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
